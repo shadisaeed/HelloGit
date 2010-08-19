661e6f316ea71cbea652733f9e3e350658bac892
shadisaeed <shadi.saeed@globalimpacsw.com>
2010-08-19 15:26:07 +0200

    second modification

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git World!");
+		for(int i=0;i<10;i++){
+			System.out.println("BAD GIT");
+		}
 	}
 
 }
